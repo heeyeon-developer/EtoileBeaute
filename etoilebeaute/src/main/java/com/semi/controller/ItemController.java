ddf3b26450217c0768a74465490f3105f6cebcc4
danbi0228 <qoeksql97@gmail.com>
2022-10-12 14:29:48 +0900

    db - shop html

--- etoilebeaute/src/main/java/com/semi/controller/ItemController.java
+++ etoilebeaute/src/main/java/com/semi/controller/ItemController.java
@@ -11,25 +11,174 @@ import com.semi.dto.ItemDTO;
 import com.semi.service.ItemService;
 
 @Controller
-@RequestMapping("/shop")
 public class ItemController {
 
-	String dir = "shop/";
-	
 	@Autowired
 	ItemService service;
 	
-	@RequestMapping("/perfumes")
+	// perfumes
+	@RequestMapping("/per_aesop")
 	public String main(Model model, Integer cateid) {
 		List<ItemDTO> list = null;
 		try {
 			list = service.itemall(101);
-			model.addAttribute("itemlist", list);
-			model.addAttribute("center", dir+"perfumes");
+			model.addAttribute("aesoplist", list);
+			model.addAttribute("center", "shop");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 		return "index";
 	}
 
+	@RequestMapping("/per_jomalone")
+	public String per_jomalone(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(102);
+			model.addAttribute("jolist", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/per_maison")
+	public String per_maison(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(103);
+			model.addAttribute("maslist", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/per_santa")
+	public String per_santa(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(104);
+			model.addAttribute("sanlist", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/per_goutal")
+	public String per_goutal(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(105);
+			model.addAttribute("golist", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	// body
+	@RequestMapping("/body_aesop")
+	public String body_aesop(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(201);
+			model.addAttribute("aesopbody", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/body_jomalone")
+	public String body_jomalone(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(202);
+			model.addAttribute("jobody", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/body_maison")
+	public String body_maison(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(203);
+			model.addAttribute("mabody", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/body_santa")
+	public String body_santa(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(204);
+			model.addAttribute("santabody", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	
+	// candle
+	@RequestMapping("/cand_aesop")
+	public String cand_aesop(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(301);
+			model.addAttribute("aesopcand", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/cand_jomalone")
+	public String cand_jomalone(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(302);
+			model.addAttribute("jocand", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	@RequestMapping("/cand_maison")
+	public String cand_maison(Model model, Integer cateid) {
+		List<ItemDTO> list = null;
+		try {
+			list = service.itemall(303);
+			model.addAttribute("maisoncand", list);
+			model.addAttribute("center", "shop");
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		return "index";
+	}
+	
+	
+	
+	
+	
+	
 }
